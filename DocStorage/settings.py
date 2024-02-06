23ae26655ad2f6fd50c5baadf1999025c2461bb6
sudipjha1205 <66268939+sudipjha1205@users.noreply.github.com>
2024-02-06 23:54:15 +0530

    Update settings.py

--- DocStorage/settings.py
+++ DocStorage/settings.py
@@ -80,18 +80,12 @@ WSGI_APPLICATION = 'DocStorage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'docstorage',
-        'USER': 'sudip',
-        'PASSWORD': 'Sud1p_jha',
-        'HOST': 'localhost',
-        'PORT': '3306',
     }
 }
-'''
+
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
