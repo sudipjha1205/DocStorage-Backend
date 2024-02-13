6bf67b3451045280676c9abaf4864b8f260e06a7
sudipjha1205 <66268939+sudipjha1205@users.noreply.github.com>
2024-02-13 13:55:50 +0530

    Update settings.py

--- DocStorage/settings.py
+++ DocStorage/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
+    'https://docstorage-9963fe18c8da.herokuapp.com',
 ]
 
 
